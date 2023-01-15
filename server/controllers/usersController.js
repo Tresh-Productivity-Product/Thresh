a313b41827cdfb87b0c8015a77f7c3af32b1f02c
Jack Sonoda <jackksono@gmail.com>
2023-01-15 15:23:25 -0800

    Updated routers/controllers

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,65 +1,6 @@
 const { data } = require('autoprefixer');
 const db = require('../models/db');
-const bcrypt = require('bcrypt')
 const usersController = {};
-const WORKFACTOR = 15;
-
-// EXAMPLE DATA:
-// {
-//     "fistName": "fname",
-//     "lastName": "lname",
-//     "password": "pass123",
-//     "userRole": "role",
-//     "email": "email"
-// }
-
-//CRYPT USER PASSWORD
-usersController.getBcrypt = (req, res, next) => {
-    console.log('req body: ', req.body)
-    //temp
-    const pass = req.body.password;
-    bcrypt.hash(pass, WORKFACTOR)
-        .then(hash => {
-            req.body.password = hash;
-            // console.log(('hash is: ', hash))
-            res.locals.user = req.body;
-            // console.log('response:', res.locals.user);
-            return next();
-        })
-}
-
-//CHECK USER PASSWORD
-usersController.checkPass = (req, res, next) => {
-    //logic to get DB user password by email
-    //store hashed pass in 'hash'
-    // console.log('req body: ', req.body)
-    const pass = req.body.password;
-    const hash = 'db result'
-    bcrypt.compare(pass, pass2)
-        .then(result => {
-            // console.log('result: ', result)
-            res.locals.signin = result; //true if success
-            return next();
-        })
-}
-
-// usersController.createUser = (req, res, next) => {
-//     // console.log(req.body)
-//     next()
-// }
-
-// const createUser = (req, res) => {
-//   const { firstName, lastName, password, userRole,email, commentId } = request.body
-
-//   pool.query('INSERT INTO users (firstName, lastName, password, userRole, email, commentId) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [firstName, lastName, password, userRole, email, commentId], (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(201).send(`User added with ID: ${results.rows[0].id}`)
-//   })
-// }
-
-
 
 //GET ALL USERS CONTROLLER
 usersController.getUsers = (req, res, next) => {
@@ -89,11 +30,11 @@ usersController.getUser = (req, res, next) => {
 usersController.createUser = (req,res,next) => {
     console.log(req.body)
      const { _id, firstName, lastName, password, userRole, email } = req.body
-    const text = `INSERT INTO users (_id, firstName, lastName, password, userRole, email) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`
+    const text = `INSERT INTO users ( _id, firstName, lastName, password, userRole, email) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`
     const values = [ _id, firstName, lastName, password, userRole, email]
     db.query(text, values)
     .then(data => {
-        console.log(data.rows)
+        //console.log(data.rows)
         res.locals.newUser = data.rows
         return next()
     })
@@ -114,9 +55,9 @@ usersController.deleteUser = (req,res,next) => {
 //UPDATE ONE USER CONTROLLER ---> not working yet
 usersController.updateUser = (req,res,next) => {
     console.log(req.body)
-     const { _id, firstName, lastName, password, userRole, email } = req.body
-    const text = `UPDATE users SET _id = $1, firstName = $2, lastName = $3, password = $4, userRole = $5, email=$6`
-    const values = [ _id, firstName, lastName, password, userRole, email]
+     const { firstName, lastName, password, userRole, email } = req.body
+    const text = `UPDATE users SET firstName = $1, lastName = $2, password = $3, userRole = $4, email=$5`
+    const values = [  firstName, lastName, password, userRole, email]
     db.query(text, values)
     .then(data => {
         console.log(data.rows)
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,8 +4,6 @@ const userController = require('../controllers/usersController');
 
 const router = express.Router();
 
-
-
 // GET ALL USERS ROUTE
 router.get('/', userController.getUsers, (req, res, next) => {
   res.status(200).json(res.locals.allUsers);
@@ -15,38 +13,16 @@ router.get('/:id', userController.getUser, (req, res, next) => {
   res.status(200).json(res.locals.oneUser);
 });
 //CREATE ONE USER ROUTE
-router.post('/:id', userController.createUser, (req, res, next) => {
+router.post('/', userController.createUser, (req, res, next) => {
   res.status(200).json(res.locals.newUser)
 });
 //DELETE ONE USER ROUTE
 router.delete('/:id', userController.deleteUser, (req, res, next) => {
-  res.status(200).json(res.locals)
+  res.status(200).json(res.locals.deleteUser)
 });
 //UPDATE ONE USER ROUTE
 router.patch('/:id', userController.updateUser, (req, res, next) => {
-  res.status(200).json(res.locals)
+  res.status(200).json(res.locals.newUser)
 });
-
-
-//CREATE USER ROUTER OPTION - RYLAN
-router.post('/signup', 
-  userController.getBcrypt,
-  // userController.createUser, 
-  (req, res, next) => {
-    // console.log(req.body)
-    res.status(200).json(res.locals.user);
-  })
-
-
-//SIGN IN ROUTER
-router.post('/signin', 
-userController.checkPass, 
-(req, res, next) => {
-  // console.log(req.body)
-  res.status(200).json(res.locals.signin);
-})
-
-
-
 
 module.exports = router;
