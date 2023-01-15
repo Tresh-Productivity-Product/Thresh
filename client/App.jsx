804b06ecff76a90778933801c27fbcee59a4286c
Kevin <kd8liu@gmail.com>
2023-01-15 12:18:42 -0800

    updated login signup page

--- client/App.jsx
+++ client/App.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import SignUp from './pages/SignUp.jsx';
-import TodoApp from './pages/TodoApp.jsx';
+import { Home } from './pages/Home.jsx';
+import { Login } from './pages/Login.jsx';
+import { SignUp } from './pages/SignUp.jsx';
+import { TodoApp } from './pages/TodoApp.jsx';
 
 const App = () => {
   const flexBetween = "flex items-center justify-between";
   return (
     <div className={`${flexBetween}`}>
       <div className={`${flexBetween}`}>
-      <Link to="/">
-        Home
-      </Link>
-      <Link to="/todoapp">
-        Dashboard
+        <Link to="/">
+          Home
+        </Link>
+        <Link to="/todoapp">
+          Dashboard
         </Link>
       </div>
-      <div>
-      <Link to="/login">
-        Login
-      </Link>
-      <Link to="/signup">
-        Signup
+      <div className={`${flexBetween}`}>
+        <Link to="/login">
+          Login
         </Link>
-        </div>
+        <Link to="/signup">
+          Signup
+        </Link>
+      </div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/todoapp' element={<TodoApp/>}/>
         <Route path='/login' element={<Login />}/>
-        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/signup' element={<SignUp />}/>
       </Routes>
     </div>
   )
--- client/pages/SignUp.jsx
+++ client/pages/SignUp.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import signUpRequest from '../api/signUpRequest';
+import { useNavigate } from 'react-router-dom';
 
-const SignUp = () => {
+export const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setfirstName] = useState('');
+  const [lastName, setlastName] = useState('');
+  const [userRole, setuserRole] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    signUpRequest(email, password)
+      .then(() => {
+        navigate('/todoapp');
+      }).catch(err => {
+        setError(err.message);
+      });
+  }
+
   return (
-    <div>signUp</div>
+    <div className="flex items-center justify-between">
+      <div style={{color: 'red'}}>{error}</div>
+        <form onSubmit={handleSignUp}>
+          Email: 
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          Password: 
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button>Sign Up</button>
+        </form>
+    </div>
   )
 }
-
-export default SignUp
