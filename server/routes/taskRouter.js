5b837d1810b226ddcebbb524a3e7d990377e84ea
RobertoRueMeloni <robertoruemeloni@gmail.com>
2023-01-15 09:57:00 -0800

    task Router and part of taskcontroller

--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -3,5 +3,26 @@ const tasksController = require('../controllers/tasksController');
 
 const router = express.Router();
 
+//GET ALL TASKS
+router.get('/', tasksController.getTasks, (req, res, next) => {
+  res.status(200).json(res.locals.allTasks);
+});
+//GET ONE TASK ROUTE
+router.get('/:id', tasksController.getTask, (req, res, next) => {
+  res.status(200).json(res.locals.oneTask);
+});
+//CREATE ONE TASK ROUTE
+router.post('/:id', tasksController.createTask, (req, res, next) => {
+  res.status(200).json(res.locals.newUser)
+});
+//DELETE ONE TASK ROUTE
+router.delete('/:id', userController.deleteTask, (req, res, next) => {
+  res.status(200).json(res.locals)
+});
+//UPDATE ONE TASK ROUTE
+router.patch('/:id', taskController.updateTask, (req, res, next) => {
+  res.status(200).json(res.locals)
+});
+
 module.exports = router;
 
